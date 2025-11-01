e5b6900b78ff9c0b901d367dd1dd7196267b5d7a
ktsu2i <kaito.tsu2i@gmail.com>
2025-11-01 20:55:44 +0900

    feat: update todo

--- api/internal/usecase/todo.go
+++ api/internal/usecase/todo.go
@@ -11,6 +11,7 @@ var ErrEmptyTitle = errors.New("title must not be empty")
 
 type ITodoUsecase interface {
 	Create(title string) (*model.Todo, error)
+	Update(id int64, title string, done bool) (*model.Todo, error)
 }
 
 type todoUsecase struct {
@@ -26,4 +27,15 @@ func (uc *todoUsecase) Create(title string) (*model.Todo, error) {
 		return nil, ErrEmptyTitle
 	}
 	return uc.repo.Create(title)
+}
+
+func (uc *todoUsecase) Update(id int64, title string, done bool) (*model.Todo, error) {
+	todo, err := uc.repo.Update(id, title, done)
+	if err != nil {
+		return nil, err
+	}
+	if todo == nil {
+		return nil, nil
+	}
+	return todo, nil
 }
